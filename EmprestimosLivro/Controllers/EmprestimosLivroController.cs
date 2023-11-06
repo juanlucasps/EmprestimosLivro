8938eb666c1bc8f814a684899bab7611df1ead83
juanl <juanlucas.paiva@hotmail.com>
2023-11-06 17:19:19 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimosLivro/Controllers/EmprestimosLivroController.cs
@@ -0,0 +1,22 @@
+﻿using EmprestimosLivro.Models;
+using EmprestimosLivro.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimosLivro.Controllers
+{
+    public class EmprestimosLivroController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimosLivroController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.EmprestimosLivro;
+            return View(emprestimos);
+        }
+    }
+}
